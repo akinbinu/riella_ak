1936d11c92bca1e8a53c9be323cb96cb3635373b
akinbinu <118278989+akinbinu@users.noreply.github.com>
2024-01-16 10:34:28 +0100

    Add files via upload
    MARVELO PROJECT ICT211

---
+++ src/MarvelData.js
@@ -0,0 +1,96 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Tony Stark",
+    alias: "Estella",
+    img1: "https://i.pinimg.com/236x/b4/3b/0c/b43b0c2346ab1aba6d21a6131a936ec6.jpg",
+    homeworld: "Earth",
+    abilities: [
+      "Beauty",
+      "Hair Model ",
+      
+    ],
+    team: "Model",
+    description:
+      "Tony Stark, also known as Iron Man, is a brilliant inventor, philanthropist, and industrialist. He uses his genius-level intellect and a highly advanced powered armor suit to fight for justice and protect the world from various threats. Stark is a charismatic playboy turned superhero who combines his technological prowess with unwavering determination to uphold the values of heroism and innovation.",
+  },
+  {
+    name: "claire",
+    alias: "Claire",
+    abilities: ["Beauty", "Hair Model"],
+    team: "model",
+    img1: "https://i.pinimg.com/564x/ab/56/e7/ab56e7093221537500f20953998514ef.jpg",
+    homeworld: "Earth",
+    description:
+      "Pepper Potts, the formidable Rescue, is an accomplished businesswoman and technological genius. She dons a powered armor suit equipped with an array of advanced technology and weaponry, allowing her to stand alongside the Avengers in the battle against supervillains and global threats. Pepper is a determined and fearless heroine with an unwavering commitment to justice.",
+  },
+  {
+    name: "Nora",
+    alias: "Nora",
+    abilities: ["Beauty", "Hair Model"],
+    team: "model",
+    img1: "https://i.pinimg.com/236x/63/79/9b/63799bc639e7a0318acf9062af2fd447.jpg",
+    homeworld: "Earth",
+    
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    name: "Beulah",
+    alias: "Beulah",
+    abilities: ["Beauty", "Hair Model"],
+    team: "Young Avengers",
+    img1: "https://i.pinimg.com/236x/84/41/f6/8441f60a88529787ee71244c0cbfec95.jpg",
+    homeworld: "Earth",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "Dera",
+    alias: "Dera",
+    abilities: ["Beauty", "Face Model"],
+    team: "Champions",
+    img1: "https://i.pinimg.com/236x/f0/ed/72/f0ed72204697056ef49dfeab2ce2d753.jpg",
+    homeworld: "Earth",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: "Pearl",
+    alias: "Pearl",
+    abilities: ["Beauty", "Face Model"],
+    team: "Agents of Atlas",
+    img1: "https://i.pinimg.com/236x/7a/2b/43/7a2b43f4c6b7e65c717bc7532c6a99e6.jpg",
+    homeworld: "Earth",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  {
+    name: "Kendall",
+    alias: "Kendall",
+    abilities: ["Beauty", "Face Model"],
+    team: "Model",
+    img1: "https://i.pinimg.com/236x/3a/af/8c/3aaf8c81fad8ea812e5bfcbe8d527b4b.jpg",
+    homeworld: "Earth",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: "Camila",
+    alias: "Camila",
+    abilities: ["Beauty", "Face Model"],
+    team: "Model",
+    img1: "https://i.pinimg.com/236x/bd/07/47/bd07478303e8ef8946c8248720d2a0e0.jpg",
+    homeworld: "Earth",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
